519b3ca98fff4e1a86fa011f023f5e1000d37cc6
bornforlamp <79363411+bornforlamp@users.noreply.github.com>
2021-11-29 19:58:10 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          Hello, I’m <strong>Shu</strong>. I’m a software engineer and a
-          translator (English/Japanese). You can contact me on{' '}
-          <a href="https://twitter.com/chibicode">Twitter</a>.
+          Hello, I’m <strong>Harshad</strong>. I’m a support engineer and a
+          translator (English/Hindi). You can contact me on{' '}
+          <a href="https://twitter.com/harshadmane">Twitter</a>.
         </p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
