1869e14927040388b9ef499757a68cd1d16d168e
bornforlamp <79363411+bornforlamp@users.noreply.github.com>
2021-11-29 20:01:53 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,6 @@ export default function Home({ allPostsData }) {
           translator (English/Hindi). You can contact me on{' '}
           <a href="https://twitter.com/harshadmane">Twitter</a>.
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
